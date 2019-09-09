0af4386266290427e3368351e78de91fd500b67f
kdenz <ksnz93@gmail.com>
2019-09-09 20:12:34 +0800

    chore(contact list page): remove red border guidelines

--- src/pages/ContactListPage/styled.ts
+++ src/pages/ContactListPage/styled.ts
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 500px;
-  border: 1px red solid;
   margin: 0 auto;
 `;
 
@@ -10,6 +9,5 @@ export const Header = styled.div`
   font-size: 2rem;
   font-weight: bold;
   padding: 1em 0;
-  border: 1px red solid;
   text-align: center;
 `;
