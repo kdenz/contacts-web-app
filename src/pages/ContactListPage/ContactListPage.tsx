a4db68cfa702692c5ab539f3528e7bad96f0430b
kdenz <ksnz93@gmail.com>
2019-09-09 15:44:31 +0800

    feat(contact list page): create initial container

---
+++ src/pages/ContactListPage/ContactListPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container, Header } from "./styled";
+
+export const ContactListPage: React.FC = () => {
+  return (
+    <Container>
+      <Header>My Contacts</Header>
+    </Container>
+  );
+};
---
+++ src/pages/ContactListPage/styled.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 500px;
+  border: 1px red solid;
+  margin: 0 auto;
+`;
+
+export const Header = styled.div`
+  font-size: 2rem;
+  font-weight: bold;
+  padding: 1em 0;
+  border: 1px red solid;
+  text-align: center;
+`;
