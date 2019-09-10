98ec93b5b97a7d798f19df877a79add4eb0f1a43
kdenz <ksnz93@gmail.com>
2019-09-11 03:05:31 +0800

    chore(contactService): add a func to generate new uuid

--- src/services/contactService.ts
+++ src/services/contactService.ts
@@ -12,11 +12,23 @@ const loadContacts = (): ContactListProps["data"] => {
     return [];
   }
 };
+
 const storeContacts = (contacts: ContactListProps["data"]) => {
   localStorage.setItem(CONTACTS_KEY, JSON.stringify(contacts));
 };
 
+// Generates new id using https://stackoverflow.com/a/44078785/3637651
+// Which 'If ID's are generated more than 1 millisecond apart, they are 100% unique.'
+const generateNewId = () => {
+  let uniqueId =
+    Math.random()
+      .toString(36)
+      .substring(2) + Date.now().toString(36);
+  return uniqueId;
+};
+
 export const contactService = {
   loadContacts,
-  storeContacts
+  storeContacts,
+  generateNewId
 };
