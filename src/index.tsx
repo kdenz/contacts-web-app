0cdd70ba720c45b964da96f319e716b982a21e21
kdenz <ksnz93@gmail.com>
2019-09-11 03:18:18 +0800

    fix(react-modal): fix a console.error for not defining app element

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import ReactModal from "react-modal";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// To fix react-modal's 'App element is not defined.' error
+// https://github.com/reactjs/react-modal/issues/576#issuecomment-394116847
+ReactModal.setAppElement("body");
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
